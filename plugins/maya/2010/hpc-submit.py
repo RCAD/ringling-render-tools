48e5dac4137ef9ebc76f5caea30870df3f1976cc
Owen Nelson <onelson@gmail.com>
2010-07-23 16:05:57 -0400

    big changes - nothing particularly noteworthy.  Just scaping along until it kinda works.

--- plugins/maya/2010/hpc-submit.py
+++ plugins/maya/2010/hpc-submit.py
@@ -2,38 +2,35 @@ from ringling.render import SubmitGui, get_version
 import maya.OpenMayaMPx as OpenMayaMPx
 kPluginCmdName="hpcSubmit"
 
+GUI = None
+
 class scriptedCommand(OpenMayaMPx.MPxCommand):
     def __init__(self):
         OpenMayaMPx.MPxCommand.__init__(self)
     
     def doIt(self,args):
-        # build the gui and display
-        gui = SubmitGui()
-        gui.show()
+        global GUI
+        GUI = None
+        GUI = SubmitGui()
         
 def cmdCreator():
-    # Create the command
-    return OpenMayaMPx.asMPxPtr( scriptedCommand() )
+    return OpenMayaMPx.asMPxPtr(scriptedCommand())
 
-# I believe this method only needs to exist if the scripted command takes args
-# since I think it's used to generate the response of MEL's "help commandName"
-# Syntax creator
-#    def syntaxCreator():
-#        syntax = OpenMaya.MSyntax()
-#        return syntax
 
-# Initialize the script plug-in
+# Runs when plug-in is enabled
 def initializePlugin(mobject):
-    mplugin = OpenMayaMPx.MFnPlugin(mobject, "Ringling", get_version(), "Any")
+    mplugin = OpenMayaMPx.MFnPlugin(mobject, "Ringling College", get_version(), "Any")
     try:
         mplugin.registerCommand( kPluginCmdName, cmdCreator )
     except:
         sys.stderr.write( "Failed to register command: %s\n" % kPluginCmdName )
         raise
-# Uninitialize the script plug-in
+# Runs when plug-in is disabled
 def uninitializePlugin(mobject):
     mplugin = OpenMayaMPx.MFnPlugin(mobject)
     try:
+        global GUI
+        del GUI
         mplugin.deregisterCommand( kPluginCmdName )
     except:
         sys.stderr.write( "Failed to unregister command: %s\n" % kPluginCmdName )
--- src/ringling/render.py
+++ src/ringling/render.py
@@ -1,12 +1,17 @@
 import os, logging, datetime
 from string import Template
-from pymel.core import SCENE, mel, workspace, sceneName, Env, window, checkBox, columnLayout, button, intField, text
+from pymel.core import SCENE, mel, workspace, sceneName, Env, window,  rowColumnLayout, frameLayout, uiTemplate, columnLayout, button, optionMenu, menuItem, intField, text, textField, scriptJob
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
 
-
 __VERSION__ = (0,0,1)
 __VERSION_TAG__ = "dev"
+
+# using this global keeps the object alive after it is created -- important!
+WINDOW = None # global tracks the GUI instance
+# these globals track the user configurable values in the GUI
+JOB_THREADS = None
+JOB_TITLE = None
 
 def get_version():
     version_string = __name__+' v'+'.'.join([str(n) for n in __VERSION__])
@@ -14,12 +19,10 @@ def get_version():
         version_string += '-%s' % __VERSION_TAG__
     return version_string
 
-HPC_SPOOL_BIN = r'C:\Users\onelson\Documents\Visual Studio 2010\Projects\hpc-spool\hpc-spool\bin\Release\hpc-spool.exe'
-
 RG = SCENE.defaultRenderGlobals
 
+HPC_SPOOL_BIN = r'hpc-spool.exe' # just use whichever is in the path first
 JOB_SCRIPT_DIR = os.path.join('D:\\', 'hpcjobs', Env().user())
-OUTPUT_DIR = '/'.join(['S:', Env().user(), 'output'])
 
 SPOOL_UNC = "//desmond/spool"
 SPOOL_LETTER = "S:"
@@ -45,77 +48,122 @@ def get_job_type():
         return 'maya_render_rman'
     return 'maya_render_sw'
  
-def get_proj_dir():
-    """workspace -q -dir;"""
-    workspace.getPath()
-
-def get_proj_name():
-    workspace.getName()
-
-def get_scene_path(norm=False):
-    if not norm:
-        return sceneName()
-    else: return os.path.normpath(sceneName())
-
 def get_scene_name():
     return os.path.splitext(os.path.basename(sceneName()))[0]
 
 def get_frame_range():
     """Returns a tuple of start and end frame numbers"""
-    return (RG.startFrame.get(), RG.endFrame.get())
-
-def build_job_script():
-    range = get_frame_range()
-    data = {
-            'date': datetime.datetime.now(),
-            'version': get_version(),
-            'job_type': get_job_type(),
-            'job_name': get_scene_name(),
-            'user': Env().user(),
-            'project_path': workspace.getPath(),
-            'output_path': None,
-            'scene_path': sceneName(),
-            'logs': SPOOL_LETTER+'/'+Env().user()+'/logs/'+get_scene_name()+'.txt', 
-            'start': int(min(range)),
-            'end': int(max(range)),
-            'threads': 8,
-            'step': int(RG.byFrameStep.get()),
-    }
-    script = INI_TEMPLATE.substitute(data).replace(SPOOL_LETTER, SPOOL_UNC
-                                                   ).replace('/', '\\') 
-    return script
-
-def submit_job(*args, **kwargs):
-    fp = write_job_script(build_job_script())
-    os.environ['HEAD_NODE'] = '216.38.182.22'
-    os.environ['PATH'] = os.path.dirname(HPC_SPOOL_BIN)
-    cmd = '%s "%s" & pause' % (os.path.basename(HPC_SPOOL_BIN), fp)
-    LOG.debug("job script:")
-    LOG.debug(open(fp).read())
-    LOG.debug(cmd)
-    os.system(cmd)
-    
-
-def write_job_script(data):
-    """
-    Creates a file with the HPC job defined inside then 
-    returns the path to the file.
-    """
-    if not os.path.isdir(JOB_SCRIPT_DIR):
-        os.makedirs(JOB_SCRIPT_DIR)
-    file_path = JOB_SCRIPT_DIR+'\\'+get_scene_name()+'_'+datetime.datetime.now().isoformat().split('.')[0].replace(':','')+'.ini'
-    with open(file_path,'w+b') as fh:
-        fh.write(data)
-    return file_path
+    return (int(RG.startFrame.get()), int(RG.endFrame.get()))
 
 
 class SubmitGui(object):
-    def show(self):
-        win = window(title="Send to HPC Cluster")
-        layout = columnLayout()
-        local_ribgen = checkBox(label = "Local Ribgen", value=True, parent=layout)
-        chunk_label = text(label="Frames per server:")
-        chunk_size = intField(value=5)
-        submit_btn = button(label="Submit", parent=layout)
-        submit_btn.setCommand(submit_job)
-        win.show()
+    window_title = "Send to HPC Cluster"
+    
+    _win = None
+    _controls = {}
+    
+    # references to key controls
+    @property
+    def job_title(self):
+        return self._controls['title'].getText()
+    @property
+    def job_threads(self):
+        return int(self._controls['threads'].getValue())
+
+    def build_job_script(self):
+        range = get_frame_range()
+        data = {
+                'date': datetime.datetime.now(),
+                'version': get_version(),
+                'job_type': get_job_type(),
+                'job_name': self.job_title,
+                'user': Env().user(),
+                'project_path': workspace.getPath(),
+                'output_path': SPOOL_LETTER+'/'+Env().user()+'/output/',
+                'scene_path': sceneName(),
+                'logs': SPOOL_LETTER+'/'+Env().user()+'/logs/'+get_scene_name()+'.*.txt', 
+                'start': min(range),
+                'end': max(range),
+                'threads': self.job_threads,
+                'step': int(RG.byFrameStep.get()),
+        }
+        script = INI_TEMPLATE.substitute(data).replace(SPOOL_LETTER, SPOOL_UNC
+                                                    ).replace(
+                                                    SPOOL_LETTER.lower(), SPOOL_UNC
+                                                    ).replace('/', '\\') 
+        return script
+    
+    def write_job_script(self, data):
+        """
+        Creates a file with the HPC job defined inside then 
+        returns the path to the file.
+        """
+        if not os.path.isdir(JOB_SCRIPT_DIR):
+            os.makedirs(JOB_SCRIPT_DIR)
+        file_path = JOB_SCRIPT_DIR+'\\'+get_scene_name()+'_'+datetime.datetime.now().isoformat().split('.')[0].replace(':','')+'.ini'
+        with open(file_path,'w+b') as fh:
+            fh.write(data)
+        return file_path
+
+
+    
+    def submit_job(self, *args, **kwargs):
+        if scene_is_dirty():
+            LOG.error("File has unsaved changes.  Save before submitting!")
+            return
+        fp = self.write_job_script(self.build_job_script())
+        cmd = '%s "%s" & pause' % (HPC_SPOOL_BIN, fp)
+        LOG.debug("job script:")
+        LOG.debug(open(fp).read())
+        LOG.debug(cmd)
+        os.system(cmd)
+        
+    def __init__(self):
+        self._win = window(title=self.window_title, resizeToFitChildren=True)
+        with self._win:
+            template = uiTemplate('HpcSubmitTemplate', force=True )
+            template.define(frameLayout, borderVisible=True, labelVisible=False)
+            template.define(text, width=75, align='right')
+            with template:
+                with columnLayout():
+                    with frameLayout():
+                        with columnLayout(width=197):
+                            with rowColumnLayout(numberOfColumns=2, columnSpacing=(2,3)):
+                                
+                                text(label="Title:")
+                                self._controls['title'] = textField(text=get_scene_name())
+                                
+                                text(label="Start Frame:")
+                                start_field = intField(editable=False, value=get_frame_range()[0])
+                                scriptJob(parent=self._win, attributeChange=[RG.startFrame, 
+                                    lambda: start_field.setValue(get_frame_range()[0])])
+                                                       
+                                text(label="End Frame:")
+                                end_field = intField(editable=False, value=get_frame_range()[1])
+                                scriptJob(parent=self._win, attributeChange=[RG.endFrame, 
+                                    lambda: end_field.setValue(get_frame_range()[1])])
+                                
+                                text(label="By Frame Step:")
+                                step_field = intField(editable=False, value=int(RG.byFrameStep.get()))
+                                scriptJob(parent=self._win, attributeChange=[RG.byFrameStep, 
+                                    lambda: step_field.setValue(int(RG.byFrameStep.get()))])
+                                
+                                text(label="Cores:")
+                                self._controls['threads'] = optionMenu()
+                                with self._controls['threads']:
+                                    menuItem(label=1)
+                                    menuItem(label=2)
+                                    menuItem(label=4)
+                                    menuItem(label=8)
+                                self._controls['threads'].setSelect(4)
+                    with frameLayout():
+                        with columnLayout(width=200):        
+                            submit_btn = button(label="Submit", width=200, height=40, align='center')
+                            submit_btn.setCommand(self.submit_job)
+        # WINDOW.show() is implied at the end of the `with WINDOW` block
+                        
+        
+    def __del__(self):
+        try: self._win.delete()
+        except: pass
+
