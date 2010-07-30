2eb7dec6c66ca17b60c7b62fd78f7234402cbaae
Owen Nelson <onelson@gmail.com>
2010-07-30 15:56:11 -0400

    maya.rman is borrowing implementation from maya.sw until we have renderman specific tasks to take care of.

--- src/ringling/hpc/maya/rman.py
+++ src/ringling/hpc/maya/rman.py
@@ -2,5 +2,13 @@
 Node Prep/Release script entry points for jobs ran with `Render.exe -r rman ...`
 """
 
-def prep():pass
-def release():pass
+from ringling.hpc.maya.sw import prep as sw_prep, release as sw_release
+"""
+Right now, we don't have any renderman specific tasks to execute, but we do
+need to make sure all the maya.sw stuff happens regardless.
+"""
+def prep():
+    sw_prep()
+
+def release():
+    sw_release()
