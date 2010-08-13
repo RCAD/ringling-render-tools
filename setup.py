28614ad4c36c3d68ceeae4bc120532633e048d99
Owen Nelson <onelson@gmail.com>
2010-08-13 11:10:14 -0400

    Added the nose test runner to setup.py so we can run a full build cycle simply during CI.  `python setup.py test` == build+test

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@ import rrt
 setup(
     name = "ringling_render_tools",
     version = rrt.__version__,
-    package_dir = {'':'src'},
-    packages = find_packages('src'),
-    extras_require = {'pymel': ['pymel>=1']},
-    dependency_links = ['http://pymel.googlecode.com/files/pymel-1.0.2.zip'],
     author = "Owen Nelson",
     author_email = "onelson@ringling.edu",
     license = "MIT",
     description = "Pipeline tools for Maya and 3DSmax rendering with Windows HPC",
     keywords = "Maya 3DSmax WindowsHPC",
+    package_dir = {'':'src'},
+    packages = find_packages('src'),
+    extras_require = {'pymel': ['pymel>=1']},
+    tests_require = ['nose'],
+    dependency_links = ['http://pymel.googlecode.com/files/pymel-1.0.2.zip'],
+    test_suite = 'nose.collector',
     include_package_data = True,
     entry_points = {
         'console_scripts': [
