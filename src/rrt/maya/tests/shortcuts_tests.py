4bd081221083c8ed6e112b6abfe68295cd21178b
Owen Nelson <onelson@gmail.com>
2010-08-16 15:14:56 -0400

    Couple more asserts for get_scene_name

--- src/rrt/maya/tests/shortcuts_tests.py
+++ src/rrt/maya/tests/shortcuts_tests.py
@@ -31,6 +31,15 @@ class TestShortcuts(unittest.TestCase):
             from pymel.core.system import sceneName
             sceneName() >> "S:/foo/bar.ext"
         self.assertEquals(get_scene_name(),'bar')
+        with Stub() as pymel.core.system:
+            from pymel.core.system import sceneName
+            sceneName() >> "S:\\foo\\bar.ext"
+        self.assertEquals(get_scene_name(),'bar')
+        
+        with Stub() as pymel.core.system:
+            from pymel.core.system import sceneName
+            sceneName() >> "S:/foo/bar_no_ext"
+        self.assertEquals(get_scene_name(),'bar_no_ext')
     
     def test_get_frame_range(self):
         with Stub() as pymel.core.general:
