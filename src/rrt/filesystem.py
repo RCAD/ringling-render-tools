5de17b676840e7279a4a278a6aa3b56750d737e3
Owen Nelson <onelson@gmail.com>
2010-09-15 16:02:54 -0400

    Spool locations will be named slightly differently on the cluster side

--- src/rrt/filesystem.py
+++ src/rrt/filesystem.py
@@ -1,4 +1,4 @@
-import ntpath
+import ntpath, re
 from subprocess import Popen, PIPE
 def get_share(path):
     """
@@ -11,7 +11,9 @@ def get_share(path):
         out = p.communicate()[0]
         if p.returncode != 0:
             raise RuntimeError("Can't find network share for drive %s" % drive_letter)
-        unc = out.splitlines()[1].split()[2].strip()
+        unc = out.splitlines()[1].split()[2].strip().lower()
+        # on the cluster side, our hosts have a 1 prepended to their names
+        re.sub("(hamming|minsky|perlis|shannon|wilkes|wilkinson)", '\g<1>1', unc, 1)
         return unc
     except Exception, e:
         raise e 
