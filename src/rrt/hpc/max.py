8870057b14fc5476c68b2a3170d35d8b6f2c5707
Owen Nelson <onelson@gmail.com>
2010-09-21 11:05:20 -0400

    building the copy/extraction prep method for max archives

--- src/rrt/hpc/max.py
+++ src/rrt/hpc/max.py
@@ -1,29 +1,26 @@
 """
 Node Prep/Release script entry points for jobs ran with `3dsmaxcmd.exe ...`
+In Max jobs, PROJECT is a path to an archive zip file, which we extract to the 
+NODE_PROJECT directory.
 """
+
+import os, tempfile, zipfile
+from shutil import copyfile
 from rrt.hpc import env
 from rrt.hpc.scripts import LOG
 ENV = env()
-import os
-
-"""
->>> import zipfile
->>> f = 'Z:\\MAX\\test.zip'
->>> z = zipfile.ZipFile
->>> z = zipfile.ZipFile(f)
->>> z.namelist()
-['test.max', 'MAXFILES.TXT']
->>> node_project = "D:\\hpc\\maxtest\\"
->>> import os
->>> os.makedirs(node_project)
->>> os.chdir(node_project)
->>> z.extractall()
->>> os.listdir(node_project)
-['MAXFILES.TXT', 'test.max']
->>> 
-"""
 
 def prep():
-    pass
+    with tempfile.NamedTemporaryFile(dir=ENV['NODE_PROJECT'], 
+                                     prefix='max') as tmp:
+        # I'm just using tempfile to generate a name, we don't want to keep 
+        # the tempfile
+        tmpzip = tmp.name
+        tmp.close() # file is deleted when closed
+    copyfile(ENV['PROJECT'], tmpzip)
+    os.chdir(ENV['NODE_PROJECT'])
+    zipfile.ZipFile(tmpzip).extractall()
+    os.remove(tmpzip)
+    # from here, we will render using %NODE_PROJECT%\\sceneName.max
 def release():
     pass
