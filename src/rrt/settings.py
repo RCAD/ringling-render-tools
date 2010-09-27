4bd2497035bcf4eb9f9c2c911e8cf8d69456dd56
Owen Nelson <onelson@ringling.edu>
2010-09-27 14:52:58 -0400

    Moving a lot of the core code from the maya gui to the JobSpec class

--- src/rrt/settings.py
+++ src/rrt/settings.py
@@ -3,6 +3,11 @@ Blessed  file system locations
 """
 
 import os
+# location to dump jobspec files
+JOBSPEC_DIR = os.path.join('D:\\', 'hpc', getpass.getuser(), 'scripts')
+
+# abspath because we can't count on it being in the PATH
+HPC_SPOOL_BIN = r'C:\Ringling\HPC\bin\hpc-spool.bat'
 
 JOB_LOGS_UNC = "\\\\clogs\\clogs"
 JOB_OUTPUT_UNC = "\\\\coutput\\coutput"
