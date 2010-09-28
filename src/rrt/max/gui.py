f0ef3211f02e2172c5aefbdc17d9d6db688de084
Owen Nelson <onelson@ringling.edu>
2010-09-28 09:19:52 -0400

    Messing with the app icon

--- src/rrt/max/gui.py
+++ src/rrt/max/gui.py
@@ -1,15 +1,13 @@
 import sys, zipfile
 from PyQt4 import QtGui
 from rrt.max.ui.submit import Ui_SubmitMainWindow
-from pkg_resources import Requirement, resource_filename
 
 class SubmitGui(QtGui.QDialog, Ui_SubmitMainWindow):
     def __init__(self, parent=None):
         super(SubmitGui, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle('hpc-submit-max')
-        icon = resource_filename(Requirement.parse("ringling-render-tools"), "rrt/extras/hpcicon3-01.png")
-        self.setWindowIcon(QtGui.QIcon(icon))
+        self.setWindowIcon(QtGui.QIcon("C:/Ringling/hpc/icons/hpcicon3-01.png"))
     
     def browse(self):
         filename = QtGui.QFileDialog.getOpenFileName(directory='Z:\\', filter="*.zip")
